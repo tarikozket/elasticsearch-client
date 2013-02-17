ec6944690c56594c1d44eb0399a59bcdfa2ba3ed
Lance Ivy <lance@cainlevy.net>
2013-02-16 21:57:21 -0500

    missing ResponseError class

--- lib/elasticsearch/error.rb
+++ lib/elasticsearch/error.rb
@@ -1,5 +1,6 @@
 module ElasticSearch
   class ConnectionFailed < StandardError; end
   class RequestError < StandardError; end
+  class ResponseError < StandardError; end
   class TimeoutError < StandardError; end
 end
